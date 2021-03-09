7a67572ac2c27c60d038e1fa5a58ef11c115b375
Preetesh21 <verma.preetesh21@gmail.com>
2021-03-10 00:23:21 +0530

    added security

--- noble_news/noble_news/settings.py
+++ noble_news/noble_news/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0(m9z^)(4ma6zq0w7c5w7b2)pvf+q6)1chzu$omeqkdvvgz3eo'
+SECRET_KEY =  env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
